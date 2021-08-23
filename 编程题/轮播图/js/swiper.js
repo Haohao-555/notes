fcda0fd8245792d4dcc957f9597f7bdd436c29bd
Haohao-555 <3283627490@qq.com>
2021-08-23 18:40:01 +0800

    更新编程题（轮播图 百度搜索框）

--- 编程题/轮播图/js/swiper.js
+++ 编程题/轮播图/js/swiper.js
@@ -2,14 +2,17 @@ var nav = document.querySelector(".nav");
 var li = document.querySelectorAll("li");
 var prev_swiper = document.querySelector(".swiper-button-left");
 var next_swiper = document.querySelector(".swiper-button-right");
-
+var swiper = document.querySelector(".swiper");
 //当前被激活的图片下标
 var nav_index = 0;
 
+// 定时器 ID
+var timeID;
+var wait = 2000
 // 还原图片透明度和小圆点
 function reduction() {
   nav.children[nav_index].classList.remove("active-nav-li");
-  li.forEach((_) => (_.classList.add("normal-img")));
+  li.forEach(_ => _.classList.add("normal-img"));
 }
 
 // 根据下标移动到相对于图片
@@ -41,6 +44,7 @@ function next_prevSwiper(index) {
 
 // 小圆点监听器
 nav.addEventListener("click", function (e) {
+  clearInterval(timeID)
   var index = e.target.dataset.index;
   if (index) {
     reduction();
@@ -48,17 +52,31 @@ nav.addEventListener("click", function (e) {
   }
 });
 
-// 定时轮播
-setInterval(function () {
-  next_prevSwiper(parseInt(nav_index) + 1);
-}, 4000);
 
+// 定时轮播
+timeID = setInterval(function () {
+  next_prevSwiper(parseInt(nav_index) + 1);
+}, wait);
+
+swiper.addEventListener("mouseout", function() {
+  timeID = setInterval(function () {
+    next_prevSwiper(parseInt(nav_index) + 1);
+  }, wait)
+ 
+})
+
+swiper.addEventListener("mouseover",function() {
+  clearInterval(timeID)
+  
+})
 // 上一张
 prev_swiper.addEventListener("click", function () {
+  clearInterval(timeID)
   next_prevSwiper(parseInt(nav_index) + 1);
 });
 
 // 下一张
 next_swiper.addEventListener("click", function () {
+  clearInterval(timeID)
   next_prevSwiper(parseInt(nav_index) - 1);
 });
