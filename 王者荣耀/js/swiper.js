a328cc845fc17aac677807e67c164fc1fcac349c
Haohao-555 <3283627490@qq.com>
2021-09-24 19:52:14 +0800

    轮播图完成

---
+++ 王者荣耀/js/swiper.js
@@ -0,0 +1,63 @@
+; (function () {
+    var timerId;
+    // 获取容器的宽度
+    var width = $('.rotate').innerWidth();
+
+    // 当前轮播图片下标
+    var index = 0;
+    var ab_index = 0;
+
+    // 轮播图
+    var $ul = $('.rotate .swiper_container');
+    var len = $ul.children().length;
+
+    // 小块
+    var $point = $(".adBtn a");
+    var adBtn = $point.length
+
+    var autoPlay = function () {
+        index++;
+        if (index > len - 1) {
+            index = 1;
+            $ul.css({ left: 0 })
+        }
+        $ul.stop(true, true).animate({ left: -(width * index) }, 500);
+
+        ab_index++
+        ab_index = ab_index > adBtn - 1 ? 0 : ab_index
+        $point.eq(ab_index).addClass("on").siblings().removeClass("on");
+    }
+
+    timerId = setInterval(function () {
+        autoPlay();
+    }, 4000);
+
+    $('.rotate').on("mouseenter", function () {
+        clearInterval(timerId)
+    });
+
+    $(".rotate").on("mouseleave", function () {
+        timerId = setInterval(function () {
+            autoPlay()
+        }, 4000);
+    })
+
+
+    $(".adBtn").on("mouseenter", "a", function (e) {
+        if (timerId) clearInterval(timerId);
+        e.stopPropagation();
+        ab_index = $(this).index();
+        index = $(this).index();
+        $ul.stop(true, true).animate({ left: -(width * index) }, 500);
+        $point.eq(ab_index).addClass("on").siblings().removeClass("on");
+    })
+
+    $(".adBtn").on("mouseleave", "a", function (e) {
+        e.stopPropagation();
+        timerId = setInterval(function () {
+            autoPlay()
+        }, 4000);
+
+
+    })
+})()
