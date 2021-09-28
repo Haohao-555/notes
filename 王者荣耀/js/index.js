10d10ecdce454cec4fefabcac12845c681fc8c0b
Haohao-555 <3283627490@qq.com>
2021-09-28 21:25:03 +0800

    王者荣耀

--- 王者荣耀/js/index.js
+++ 王者荣耀/js/index.js
@@ -1,5 +1,8 @@
 $(function () {
-    var nav_flag = false; // true 已经显示 false 已经隐藏
+
+    var nav_flag = false; // true 已经显示 false 
+    var big = false;
+    var audio = false;
     $(".header .nav").on("mouseenter", "li", function () {
         var len = $(".header .nav").children().length;
         var index = $(this).index()
@@ -8,8 +11,45 @@ $(function () {
             $li.addClass("h_active").siblings().removeClass("h_active");
             $li.find("a em").addClass("f_active").siblings().removeClass("f_active")
         }
-    //    if(!nav_flag) {
-    //        $(".J_subNav").stop(true, true).animate({height: 285}, 500);
-    //    }
+
     })
+    $(".header .nav").on("mouseleave", "li", function () {
+        $(this).removeClass("h_active").find("a em").removeClass("f_active");
+    })
+    $(document).on("mousemove", function (e) {
+        var max;
+        var min = $(".banner")[0].offsetTop;
+        var v = $(e)[0].pageY;
+        max = nav_flag ? 410 : 125;
+        if (!big && !audio) {
+            if (v >= min && v <= max) {
+                $(".J_subNav").slideDown(300);
+                nav_flag = true
+            } else {
+                $(".J_subNav").slideUp(300);
+                nav_flag = false
+            }
+        }
+    })
+
+    $(".ost_left .l_item-2").on("mouseenter", function () {
+        if (nav_flag) $(".J_subNav").slideUp(300);
+        big = true;
+        $(".ost_d").css({ display: "block" });
+    })
+    $(".ost_d").on("mouseleave", function () {
+        $(".ost_d").css({ display: "none" });
+        big = false;
+    })
+
+    // $(".ost_right .r_item_2").on("mouseenter", function () {
+    //     if (nav_flag) $(".J_subNav").slideUp(300);
+    //     $(".ost_pop").css({ display: "block" });
+    //     audio = true;
+    // })
+    // $(".ost_pop").on("mouseleave", function () {
+       
+    //     $(".ost_pop").css({ display: "none" });
+    //     audio = false;
+    // })
 })
