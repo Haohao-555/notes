f1d20b9728b0f949c4ba1c79ed3542ebe0862059
Haohao-555 <3283627490@qq.com>
2021-11-23 18:41:06 +0800

    封装防抖函数

---
+++ 练习/防抖节流/debounce1.js
@@ -0,0 +1,30 @@
+var box = document.querySelector(".box")
+function getUserAction(event) {
+    console.log("当前的this指向（对象元素）", this)
+    console.log("event事件对象", event)
+    event.target.innerHTML = ++event.target.innerHTML
+}
+function debounce(func, wait, imm) {
+    var timerId;
+     var debounced = function(event) {
+         var e = event || window.event;
+         if(timerId) {
+             clearInterval(timerId)
+         }
+         func.bind(this)
+         if(imm) {
+             if(!timerId) {
+                 func(e)
+             }
+             timerId = setTimeout(function() {
+                 timerId = null
+             }, wait)
+         }else {
+             timerId = setTimeout(function() {
+                  func(e)
+             }, wait)
+         }
+     }
+     return debounced
+}
+box.onmousemove = debounce(getUserAction, 1000, true)
